d3e26bd8236e64c1d168f421dad43999ac358560
Phofers <adamkuc915@gmail.com>
2023-11-27 17:19:54 -0600

    first commit

---
+++ Calculator.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner calScan = new Scanner(System.in);
+
+        System.out.println("Term One: ");
+        String inputTermOne = calScan.nextLine();
+
+        System.out.println("Operator (+, -, *, or /): ");
+        String operator = calScan.nextLine();
+
+        System.out.println("Term Two: ");
+        String inputTermTwo = calScan.nextLine();
+
+        calScan.close();
+        
+        double inputOne = Double.parseDouble(inputTermOne);
+        double inputTwo = Double.parseDouble(inputTermTwo);
+
+        double result = 0;
+        
+        switch (operator) {
+            case "+":
+                result = inputOne + inputTwo;
+                break;
+            case "-": 
+                result = inputOne - inputTwo;
+                break;
+            case "*":
+                result = inputOne * inputTwo;
+                break;
+            case "/":
+                result = inputOne / inputTwo;
+                break;
+            default:
+                System.out.println("Something went wrong! Please try again.");
+                
+        }
+        
+        System.out.println(result);
+
+    }
+}
